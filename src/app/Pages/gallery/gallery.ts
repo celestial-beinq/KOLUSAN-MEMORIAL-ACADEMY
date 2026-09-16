21d42a50e9566bfc8e4f6d281c4ee99144cc0b85
Idowu60 <idowublessing600@gmail.com>
2026-09-16 22:05:03 +0800

    NEW KOLUSAN MEMORIAL HIGH SCHOOL

--- src/app/Pages/gallery/gallery.ts
+++ src/app/Pages/gallery/gallery.ts
@@ -73,7 +73,7 @@ export class GalleryPage {
       categoryLabel: 'SPORTS ARENA',
       image: 'images/campus-sports.jpg',
       description:
-        'Intense competition and sportsmanship on our FIFA-dimension pitch during the annual JON BITRICE COLLEGE Games.',
+        'Intense competition and sportsmanship on our FIFA-dimension pitch during the annual KOLUSAN MEMORIAL HIGH SCHOOL  Games.',
       badge: 'Athletics Day',
     },
     {
@@ -123,7 +123,7 @@ export class GalleryPage {
       categoryLabel: 'COURT SPORTS',
       image: 'images/IMG_4342.JPG',
       description:
-        'JON BITRICE COLLEGE basketball squad displaying tactical precision and agility on our outdoor courts.',
+        'KOLUSAN MEMORIAL HIGH SCHOOL  basketball squad displaying tactical precision and agility on our outdoor courts.',
       badge: 'Championship Match',
     },
     {
